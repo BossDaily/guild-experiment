4a0e955d47377884e491e2b79a04292286378032
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-03-27 15:58:14 +0000

    added map of all experiments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 import Image from "next/image";
 import NavBar from "../components/NavBar";
+import ExperimentCard from "@/components/Experiment/ExperimentCard";
 
-export default function Home() {
+const experimentData = async () => {
+  const res = await fetch("https://api.rollouts.advaith.io/");
+
+  return res.json();
+};
+
+export default async function Home() {
+  const experiments = await experimentData();
+
   return (
     <main>
       <div>
         <NavBar />
-        <h1>Test</h1>
+        {experiments.reverse().map((experiment) => (
+          <ExperimentCard
+            key={experiment.data.id}
+            id={experiment.data.id}
+            name={experiment.data.title}
+          />
+        ))}
       </div>
     </main>
   );
