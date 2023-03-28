59afc1e5f521a75712a450485426218862c0140b
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-03-28 14:22:07 +0000

    queries experiment object

--- src/app/experiment/[id]/page.tsx
+++ src/app/experiment/[id]/page.tsx
@@ -3,12 +3,21 @@ import path from "node:path";
 import { ReactNode } from "react";
 import fs, { read, readdirSync } from "fs";
 
+const experimentData = async () => {
+  const res = await fetch("https://api.rollouts.advaith.io/");
+  return res.json();
+};
+
+
 
 
 export default async function Home({ params }) {
-  
-
   const { id } = params;
 
-  return <div className="text-white">Experiment: {id}</div>;
+  const experiments = await experimentData();
+  const experiment = await experiments.find((experiment) => experiment.data.hash == id);
+  console.log(experiment)
+  
+
+  return <div className="text-white">Experiment: {experiment.data.title}</div>;
 }
