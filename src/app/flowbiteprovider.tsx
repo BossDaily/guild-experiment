6cc27dba8811384eee48a2f03a192a40eab84bf8
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-03-31 15:59:47 +0000

    mroe duck tape

--- src/app/flowbiteprovider.tsx
+++ src/app/flowbiteprovider.tsx
@@ -2,8 +2,9 @@
 
 import { Flowbite } from "flowbite-react";
 import { customtheme as theme } from "../app/theme";
+import { ReactNode } from "react";
 
 
-export default function Provider({ children }) {
+export default function Provider({ children }: {children: ReactNode}) {
   return <Flowbite theme={{ theme }}>{children}</Flowbite>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,9 @@ export default async function Home() {
       <div>
         <NavBar />
         <div className="grid grid-cols-4 gap-4">
-          {experiments.reverse().map((experiment) => (
+          {
+            // @ts-ignore
+          experiments.reverse().map((experiment) => (
             <ExperimentCard
               key={experiment.data.id}
               id={experiment.data.hash}
