0a399f0abfb074d4940652f7abcc25d685aea094
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-04-03 13:47:45 +0000

    I forgor return statement

--- src/lib/checkGuild.ts
+++ src/lib/checkGuild.ts
@@ -2,5 +2,6 @@
 export async function checkGuild(exp: string, id: string) {
   const checkExperiment = await fetch(`https://experiments.dscrd.workers.dev/experiments/check/${exp}/${id}`).then(res => { return res.json() }).catch((err) => { return 'there was an error' });
 
+  return checkExperiment
 
 }
