2d56aa2925a0c83bc94e7f006efb92c0fb9d05bf
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-04-04 16:36:50 +0000

    metadata for favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,24 @@ import NavBar from "../components/NavBar";
 import ExperimentCard from "@/components/Experiment/ExperimentCard";
 import ExperimentContainer from "@/components/Experiment/ExperimentContainer";
 import { Experiment } from "../../experiment";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 
+
+export async function generateMetadata(): Promise<Metadata> {
+  const session = await getServerSession(authOptions);
+  let avatar = `https://media.discordapp.net/attachments/996891985157828638/1084314805407584296/cow.gif?size=20`
+  if (session) {
+    avatar = `${session.discordUser.image_url}?size=20`
+  }
+  return {
+    icons: {
+      icon: avatar
+    }
+  };
+}
 const experimentData = async (): Promise<Experiment[]> => {
   const res = await fetch("https://api.rollouts.advaith.io/", {
     next: { revalidate: 100 },
