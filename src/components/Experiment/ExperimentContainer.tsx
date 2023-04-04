2c769221fa70062801ae3c42b3d27e0ed31e6537
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-04-04 15:21:21 +0000

    container is flex, and added a search bar

--- src/components/Experiment/ExperimentContainer.tsx
+++ src/components/Experiment/ExperimentContainer.tsx
@@ -1,28 +1,42 @@
-'use client'
+"use client";
 
 import ExperimentCard from "@/components/Experiment/ExperimentCard";
 import { Experiment } from "../../../experiment";
+import { useState } from "react";
+import { TextInput } from "flowbite-react";
 
-export default async function Home({exp}: {exp: Experiment[]}) {
+export default function Home({ exp }: { exp: Experiment[] }) {
   const experiments = exp;
-  
+
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    const lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
+  const expFiltered = experiments.filter((exp) => {
+    return exp.data.title.toLowerCase().includes(inputText);
+  });
 
   return (
-    <main>
-      <div>
-        <div className="justify-center grid grid-rows-1 gap-4 md:grid-cols-4 px-4">
-          {
-            // @ts-ignore
-          experiments.reverse().map((experiment) => (
+    <div className="flex flex-col">
+      <div className="self-center flex-none max-w-md pb-4">
+        <TextInput onChange={inputHandler} className="" />
+      </div>
+      <div className="justify-center grid grid-rows-1 gap-4 md:grid-cols-2 lg:grid-cols-4 px-4">
+        {
+          // @ts-ignore
+          expFiltered.reverse().map((experiment) => (
             <ExperimentCard
               key={experiment.data.id}
               id={experiment.data.hash}
               name={experiment.data.title}
               description={experiment.data.description}
             />
-          ))}
-        </div>
+          ))
+        }
       </div>
-    </main>
+    </div>
   );
 }
