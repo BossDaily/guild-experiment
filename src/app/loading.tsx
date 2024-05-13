6e11c2feae16d04ddb2fbd08a032222f2d721a6c
BossDaily <gboys2008@gmail.com>
2024-05-13 16:45:19 -0500

    Updated cow loading gif

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,10 +4,11 @@ export default function Loading() {
   return (
     <div className="flex justify-center items-center h-screen">
       <Image
-        src="https://media.discordapp.net/attachments/996891985157828638/1084314805407584296/cow.gif?size=256"
+        src="/cow.gif"
         height={256}
         width={256}
         alt="cowloading"
+        unoptimized
       />
     </div>
   );
